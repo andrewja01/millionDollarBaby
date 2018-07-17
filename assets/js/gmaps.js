d625eefe1f9ba84586078a3a888e5880daea82b2
Christian Martinez <christianmpacheco@gmail.com>
2018-07-16 21:49:25 -0700

    working and listed

--- assets/js/gmaps.js
+++ assets/js/gmaps.js
@@ -1,99 +1,19 @@
-
 var geocoder;
 var map;
+var mapOptions;
    function initMap() {
    geocoder = new google.maps.Geocoder();
-     var mapOptions = {
+     mapOptions = {
        zoom: 12,
-       center: new google.maps.LatLng(51.5, -0.12),
-       mapTypeId: google.maps.MapTypeId.HYBRID,    
-       styles: [
-        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [{color: '#263c3f'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#6b9a76'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{color: '#38414e'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#212a37'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9ca5b3'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [{color: '#746855'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#1f2835'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#f3d19c'}]
-        },
-        {
-          featureType: 'transit',
-          elementType: 'geometry',
-          stylers: [{color: '#2f3948'}]
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{color: '#17263c'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#515c6d'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.stroke',
-          stylers: [{color: '#17263c'}]
-        }
-      ]   
-      }
+       center: {lat: 50.7944, lng: -122.45405}
+   }
      map = new google.maps.Map(document.getElementById('map'), mapOptions);
    }
  
-   function codeAddress() {
-     var address = document.getElementById('address').value;
-     geocoder.geocode( { 'address': address}, function(results, status) {
+   function codeAddress(arr) {
+     //var address = document.getElementById('city').value;
+     var address = arr;
+     geocoder.geocode( {'city': address}, function(results, status) {
        if (status == 'OK') {
          map.setCenter(results[0].geometry.location);
          var marker = new google.maps.Marker({
@@ -108,5 +28,9 @@ var map;
    }
    
    $("#btnGetAdd").on('click', function(){
-       codeAddress();
+      codeAddress();
+      lats = 37.783058;
+      longs = -122.451649;
+      map.setCenter(new google.maps.LatLng(lats, longs));
    });
+
--- datafinit.js
+++ datafinit.js
@@ -1,61 +1,89 @@
-var queryURL = "https://api.datafiniti.co/v4/properties/search";
-
-var data = JSON.stringify({
-    format: "json", 
-    num_records:10, 
-    download: false,
-    query:"postalCode:94118 AND prices:*"
-
+$("#btnGetAdd").on("click", function () {
+    var city = $("#city").val();
+    var zip = $("#zip").val();
+    var state = $("#state").val();
+    var rent = $("#monthlyRent").val();
+    // var long = 
+    // var lat =
+    //var gmapAddress = city + ", " + state;
+    //dynamically entering address to be searched by google maps
+    //$("#address").text(city + ", " + state);
+    var queryURL = "https://api.datafiniti.co/v4/properties/search";
+    var data = JSON.stringify({
+        format: "json",
+        num_records: 10,
+        download: false,
+        query: "city:" + city + " AND province:CA AND postalCode:" + zip + " AND prices:* AND propertyTaxes:* AND mlsNumber:*"
+    });
+    $.ajax({
+        type: "POST",
+        url: queryURL,
+        dataType: "json",
+        data: data,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc19hZG1pbiI6ZmFsc2UsInN1YiI6IjU2NSIsImlzcyI6ImRhdGFmaW5pdGkiLCJlbWFpbCI6ImFuZHJld2phMDFAZ21haWwuY29tIn0.07qQQ0hIJBabUMUwOicDlzjAc7jgn-k-dC5PC9OXHh8');
+        }
+    }).then(function (response) {
+        console.log(response);
+        var sumROI = 0;
+        var sumPurchase = 0;
+        var results = response.records;
+        rent = rent * 12;
+        //var arr = [];
+        
+        //clearing out the table body
+        $("tbody").empty();
+        for (var i = 0; i < results.length; i++) {
+            //variable for property management fees
+            var propMgmt = rent * .08;
+            //variable for annual insurance fees
+            var propInsurance = 1000;
+            //variables for dynamically creating the table rows 
+            var tBody = $("tbody");
+            var tRow = $("<tr>");
+            
+            // //var strAddress = results[i].address + results[i].city + results[i].province + results[i].postalCode;
+            // // var strAddress = "46540 fremont blvd fremont,ca 94538";
+            // // var url = "";
+            // var listing = "<a href onclick="javascript:goToURL(google.com)">Go To URL</a>";
+            // //var listing = "<a id='code' href= # OnClick=javascript:codeAddress('" + strAddress + "')>" + results[i].mlsNumber + "</a>";
+            // //"<href=codeAddress('" + strAddress + "')>" + results[i].mlsNumber + "</a>";
+            // function goToURL(url){
+            //     window.open("google.com");
+                
+            //     }
+            // console.log(listing);
+            var listing = $("<td>").html(results[i].mlsNumber);
+            var prop = $("<td>").html(results[i].address + ", " + results[i].city + ", " + results[i].province + " " + results[i].postalCode);
+            var taxes = $("<td>").html("$" + (results[i].propertyTaxes[0].amount).toFixed(2));
+            var purchPrice = $("<td>").html("$" + (results[i].prices[0].amountMax).toFixed(2));
+            //calculating return
+            var roi = $("<td>").html(Math.round((rent - results[i].propertyTaxes[0].amount - propInsurance - propMgmt) / (results[i].prices[0].amountMax) * 100) + "%");
+            //calculating average return in that city/ zipcode
+            var sumROI = sumROI + Math.round((rent - results[i].propertyTaxes[0].amount - propInsurance - propMgmt) / (results[i].prices[0].amountMax) * 100);
+            console.log(sumROI);
+            //calculating average purchase price in that city/ zipcode
+            var sumPurchase = sumPurchase + (results[i].prices[0].amountMax);
+            console.log(sumPurchase);
+            console.log(results[i].latitude);
+            console.log(results[i].longitude);
+            //appending to the DOM
+            tRow.append(listing, prop, roi, taxes, purchPrice);
+            tBody.append(tRow);
+            // var placeholder =({//info: results[i].address + ", " + results[i].city + ", " + results[i].province + " " + results[i].postalCode,
+            //             lat: results[i].latitude,
+            //             lng: results[i].longitude
+            //         });
+            // console.log(arr);
+        }
+        //appending averages to the DOM
+        var sumROI = sumROI / 10;
+        $("#spnROI").empty();
+        $("#spnROI").append(sumROI + "%");
+        console.log(sumROI);
+        var sumPurchase = sumPurchase / 10;
+        $(".average").empty();
+        $(".average").append("$" + (sumPurchase.toFixed(2)));
+    });
 });
-
-$.ajax({
-    type: "POST",
-    url: queryURL,
-    dataType: "json",
-    data: data,
-    contentType: "application/json",
-    beforeSend: function (xhr) {
-        xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc19hZG1pbiI6ZmFsc2UsInN1YiI6IjU2NSIsImlzcyI6ImRhdGFmaW5pdGkiLCJlbWFpbCI6ImFuZHJld2phMDFAZ21haWwuY29tIn0.07qQQ0hIJBabUMUwOicDlzjAc7jgn-k-dC5PC9OXHh8');
-    }
-}) .then(function(response) {
-    console.log(response);
-    console.log("hello world");
-}); 
-
-  
-//   var xhr = new XMLHttpRequest();
-//   xhr.withCredentials = true;
-//   xhr.send(null);
-  
-//   xhr.addEventListener("readystatechange", function () {
-//     if (this.readyState === this.DONE) {
-//       console.log(this.responseText);
-//     }
-//   });
-  
-//   xhr.open("GET", "https://api.datafiniti.co/v4/properties/search");
-//   xhr.setRequestHeader("authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc19hZG1pbiI6ZmFsc2UsInN1YiI6IjU2NSIsImlzcyI6ImRhdGFmaW5pdGkiLCJlbWFpbCI6ImFuZHJld2phMDFAZ21haWwuY29tIn0.07qQQ0hIJBabUMUwOicDlzjAc7jgn-k-dC5PC9OXHh8");
-// //   xhr.setRequestHeader("Access-Control-Allow-Credentials", true);
-//   xhr.send(null);
-
-
-// var data = JSON.stringify({
-//     "query": "Fremont",
-//     "format": "json",
-//     "download": "false"
-//     });
-    
-//     var xhr = new XMLHttpRequest();
-//     xhr.withCredentials = true;
-    
-//     xhr.addEventListener("readystatechange", function () {
-//     if (this.readyState === this.DONE) {
-//         console.log(this.responseText);
-//     }
-//     });
-    
-//     xhr.open("POST", "https://api.datafiniti.co/v4/properties/search");
-//     xhr.setRequestHeader("authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc19hZG1pbiI6ZmFsc2UsInN1YiI6IjU2NSIsImlzcyI6ImRhdGFmaW5pdGkiLCJlbWFpbCI6ImFuZHJld2phMDFAZ21haWwuY29tIn0.07qQQ0hIJBabUMUwOicDlzjAc7jgn-k-dC5PC9OXHh8");
-    
-//     xhr.send(data);
-
